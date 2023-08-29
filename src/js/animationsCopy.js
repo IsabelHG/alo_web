b39f562b5644509cb88422d8c433aadbce4c18f2
Isabel Higuita <isabel.higuita@interservicios.com>
2023-08-29 00:21:10 -0500

    Cambio Color del Menú

--- src/js/animationsCopy.js
+++ src/js/animationsCopy.js
@@ -20,6 +20,115 @@ if (window.innerWidth < 1024) {
   }
 }
 
+function obtenerPosicion(elemento) {
+  let x = 0; // posición horizontal
+  let y = 0; // posición vertical
+  while (elemento) { // mientras el elemento tenga un padre
+    x += elemento.offsetLeft; // sumar la distancia horizontal del elemento al padre
+    y += elemento.offsetTop; // sumar la distancia vertical del elemento al padre
+    elemento = elemento.offsetParent; // asignar el padre como el nuevo elemento
+  }
+  return { x: x, y: y }; // devolver un objeto con las coordenadas
+}
+
+let elementoHome = document.getElementById("home"); // obtener el elemento por su id
+let posicionHome = obtenerPosicion(elementoHome); // llamar a la función para obtener su posición
+let elementoWeAre = document.getElementById("weareline"); // obtener el elemento por su id
+let posicionWeAre = obtenerPosicion(elementoWeAre); // llamar a la función para obtener su posición
+let elementoWeDo = document.getElementById("wedo"); // obtener el elemento por su id
+let posicionWeDo = obtenerPosicion(elementoWeDo); // llamar a la función para obtener su posición
+let elementoOurPassions = document.getElementById("ourpassions"); // obtener el elemento por su id
+let posicionOurPassions = obtenerPosicion(elementoOurPassions); // llamar a la función para obtener su posición
+let elementoWeConnect = document.getElementById("weconnect"); // obtener el elemento por su id
+let posicionWeConnect = obtenerPosicion(elementoWeConnect); // llamar a la función para obtener su posición
+let elementoFinal = document.getElementById("animationFinal"); // obtener el elemento por su id
+let posicionFinal = obtenerPosicion(elementoFinal); // llamar a la función para obtener su posición
+
+let menuBar = document.getElementById("footer-nav");
+
+const instaIcon = document.getElementById("insta-icon");
+const lkdinIcon = document.getElementById("lkdin-icon");
+const mediumIcon = document.getElementById("medium-icon");
+const twitIcon = document.getElementById("twit-icon");
+const logoNav = document.getElementById("logoNav");
+
+function colorWhite() {
+  menuBar.classList.add("bg-aloWhite");
+  menuBar.classList.remove("bg-aloBrown");
+}
+
+function colorBrown() {
+  menuBar.classList.add("bg-aloBrown")
+  menuBar.classList.remove("bg-aloWhite");
+  menuBar.classList.remove("bg-aloGreen");
+  menuBar.classList.remove("bg-aloGray");
+}
+
+function colorGreen() {
+  menuBar.classList.add("bg-aloGreen")
+  menuBar.classList.remove("bg-aloBrown");
+  menuBar.classList.remove("bg-aloWhite");
+}
+
+function colorGray() {
+  menuBar.classList.add("bg-aloGray")
+  menuBar.classList.remove("bg-aloWhite");
+  menuBar.classList.remove("bg-aloGreen");
+}
+
+function textGray() {
+  menuBar.classList.remove("text-white")
+  menuBar.classList.remove("border-aloWhite")
+  logoNav.classList.remove("fill-white");
+  instaIcon.classList.remove("fill-white");
+  lkdinIcon.classList.remove("fill-white");
+  mediumIcon.classList.remove("fill-white");
+  twitIcon.classList.remove("fill-white");
+  menuBar.classList.add("text-aloGray")
+  menuBar.classList.add("border-aloGray")
+  logoNav.classList.add("fill-aloGray");
+  instaIcon.classList.add("fill-aloGray");
+  lkdinIcon.classList.add("fill-aloGray");
+  mediumIcon.classList.add("fill-aloGray");
+  twitIcon.classList.add("fill-aloGray");
+}
+
+function textWhite() {
+  menuBar.classList.add("text-white")
+  menuBar.classList.add("border-aloWhite")
+  logoNav.classList.add("fill-white");
+  instaIcon.classList.add("fill-white");
+  lkdinIcon.classList.add("fill-white");
+  mediumIcon.classList.add("fill-white");
+  twitIcon.classList.add("fill-white");
+  menuBar.classList.remove("text-aloGray")
+  menuBar.classList.remove("border-aloGray")
+  logoNav.classList.remove("fill-aloGray");
+  instaIcon.classList.remove("fill-aloGray");
+  lkdinIcon.classList.remove("fill-aloGray");
+  mediumIcon.classList.remove("fill-aloGray");
+  twitIcon.classList.remove("fill-aloGray");
+}
+
+window.addEventListener("wheel", function (e) {
+  if (window.scrollX >= posicionFinal.x) {
+    colorGray();
+    textWhite();
+  }else if (window.scrollX >= posicionOurPassions.x) {
+    colorWhite();
+    textGray();
+  }else if (window.scrollX >= posicionWeDo.x) {
+    colorGreen();
+    textGray();
+  }else if (window.scrollX >= posicionWeAre.x) {
+    colorBrown();
+    textGray();
+  }else{
+    colorWhite();
+    textGray();
+  }
+});
+
 if (window.innerWidth >= 1024) {
   var item = document.getElementById("MAIN");
 
